2a074cd3e85386b02a36f0031cc6404b38adee0e
Kaiky Sousa Paes <171882322+kaiky-paes@users.noreply.github.com>
2026-04-15 11:03:04 -0300

    Add files via upload

---
+++ aula01/src/Main.java
@@ -0,0 +1,18 @@
+public class Main { //Estrutura do programa
+    public static void main(String[] args) { //Onde executa tudo
+        /*Declaração de variáveis
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2) / 2; //Tipo de dado decimais
+        System.out.println("A média é igual a " + m); //Comando de saída | concatenar + */
+        int n = 10;
+        System.out.println(n);
+        n++; //Incremento
+        System.out.println(n);
+        n--; //Decremento
+        System.out.println(n);
+        int x = 4;
+        x += 2;
+        System.out.println(x);
+        } //Fechamento do public static
+} //Fechamento da estrutura
